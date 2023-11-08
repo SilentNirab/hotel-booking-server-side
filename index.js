23babfd9ec1c5b2fb4eb65edbf1f13b542590232
Md Nirab <nabilnirab255@gmail.com>
2023-11-09 01:23:32 +0600

    updade api added

--- index.js
+++ index.js
@@ -68,8 +68,14 @@ async function run() {
 
     app.post('/booking', async(req, res) =>{
       const booking = req.body;
-      console.log(booking);
       const result = await bookingCollection.insertOne(booking);
+      res.send(result)
+    })
+
+    app.delete('/booking/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bookingCollection.deleteOne(query);
       res.send(result)
     })
 
