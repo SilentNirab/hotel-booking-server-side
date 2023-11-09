b670cebc8fc4044e1bb79787bb7d9265fa358640
Md Nirab <nabilnirab255@gmail.com>
2023-11-09 12:19:26 +0600

    auth api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -53,6 +54,13 @@ async function run() {
       const query = {_id: new ObjectId(id) }
       const result = await roomsCollection.findOne(query);
       res.send(result)
+    })
+
+    //auth api
+    app.post('/jwt', async(req, res) =>{
+      const user = req.body;
+      console.log('user token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
     })
 
     // Booking api
